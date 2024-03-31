2ccb32ab158144ea5293edbdf71e17bccb174bb8
Thomas Donahue <donahue777@yahoo.com>
2024-03-31 15:56:03 -0400

    Challenge 5 done

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,7 +9,25 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            bool containsWord = false;
+
+            if (string.Equals(words, null) || words.Contains(null))
+            {
+                return false;
+            }
+
+            if (ignoreCase == true)
+            {
+                word = word.ToLower();
+                List<string> lc = words.Select(x => x.ToLower()).ToList();
+                containsWord = lc.Contains(word);
+            }
+
+            if (ignoreCase == false)
+            {
+                containsWord = words.Contains(word);
+            }
+            return containsWord;
         }
 
         public bool IsPrimeNumber(int num)
